cc94b04a851aec6a8f5a67f31a2b628f85d3e54b
SuryodayBasak <suryodaybasak@gmail.com>
2018-01-16 09:43:41 +0530

    grouped different observations of the same objects into dictionaries

--- grouping-proto.py
+++ grouping-proto.py
@@ -18,13 +18,23 @@ for index, row in df.iterrows():
         if (row[par] in grp_id[j]) or (row[nbh] in grp_id[j]):
             if row[par] not in grp_id[j]:
                 grp_id[j].append(row[par])
-                grp[j].append([row[par], row['x1'], row['y1'], row['z1'],
-                                row['f1'], row['n1']])
+                #grp[j].append([row[par], row['x1'], row['y1'], row['z1'],
+                #                row['f1'], row['n1']])
+                grp[j].append({"id" : row[par],
+                                "x" : row['x1'],
+                                "y" : row['y1'],
+                                "z" : row['z1'],
+                                "fuv" : row['f1'],
+                                "nuv" : row['n1']})
 
             if row[nbh] not in grp_id[j]:
                 grp_id[j].append(row[nbh])
-                grp[j].append([row[nbh], row['x2'], row['y2'], row['z2'],
-                                row['f2'], row['n2']])
+                grp[j].append({"id" : row[nbh],
+                                "x" : row['x2'],
+                                "y" : row['y2'],
+                                "z" : row['z2'],
+                                "fuv" : row['f2'],
+                                "nuv" : row['n2']})
 
             flag = 1
     if flag == 0:
@@ -34,21 +44,44 @@ for index, row in df.iterrows():
         grp_id.append(new_grp_id)
 
         new_grp = []
-        new_grp.append([row[par], row['x1'], row['y1'], row['z1'],
-                        row['f1'], row['n1']])
-        new_grp.append([row[nbh], row['x2'], row['y2'], row['z2'],
-                        row['f2'], row['n2']])
+        new_grp.append({"id" : row[par],
+                        "x" : row['x1'],
+                        "y" : row['y1'],
+                        "z" : row['z1'],
+                        "fuv" : row['f1'],
+                        "nuv" : row['n1']})
+        new_grp.append({"id" : row[nbh],
+                        "x" : row['x2'],
+                        "y" : row['y2'],
+                        "z" : row['z2'],
+                        "fuv" : row['f2'],
+                        "nuv" : row['n2']})
         grp.append(new_grp)
 
 for i in range(len(grp_id)):
     print('Group by IDs', i+1, ' = ', grp_id[i])
 
 for i in range(len(grp)):
-    #print('Group ', i+1, ' = ', grp[i])
+    grp_json = {}
+    grp_json["obj_id"] = "grp"+str(i)
+    grp_json["objects"] = grp[i]
+
+    n = len(grp_json["objects"])
+    grp_json["eff_fuv"] = sum(item["fuv"] for item in grp_json["objects"])/n
+    grp_json["eff_nuv"] = sum(item["nuv"] for item in grp_json["objects"])/n
+    grp_json["eff_x"] = sum(item["x"] for item in grp_json["objects"])/n
+    grp_json["eff_y"] = sum(item["y"] for item in grp_json["objects"])/n
+    grp_json["eff_z"] = sum(item["z"] for item in grp_json["objects"])/n
+
+    print(grp_json)
+    print('\n')
+    """
+    print('Group ', i+1, ' = ', grp[i])
     print('\nGroup', i+1)
     print('Samples:')
     for j in range(len(grp[i])):
         print('Sample', j+1, ': ', grp[i][j])
+    """
 
 """
 for i in range(len(data)):
