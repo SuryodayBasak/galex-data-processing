72ad61e9cd6ff6affe2af1b0c176933c1d38b6ae
SuryodayBasak <suryodaybasak@gmail.com>
2018-01-26 05:38:13 +0530

    grouping prototype is ready

--- grouping-proto.py
+++ grouping-proto.py
@@ -1,62 +1,95 @@
 import pandas as pd
+import json
 
-df = pd.read_csv("../data/redundantSamples_86_specter.csv")
-par = 'id1'
-nbh = 'id2'
+df = pd.read_csv("../data/distanceMatches_specter.csv",
+                    dtype={'obsid1': object, 'obsid2': object,
+                            'peid1': object, 'peid2': object})
 
 grp_id = []
+grp_edges = []
 grp = []
 count = 0
 
 for index, row in df.iterrows():
-    if index > 1000:
+    if index > 10000:
         break
     count += 1
-    print("Currently on row: ", count)
+    print("Currently on row: ", count, "/ 154924")
     flag = 0
     for j in range(len(grp_id)):
-        if (row[par] in grp_id[j]) or (row[nbh] in grp_id[j]):
-            if row[par] not in grp_id[j]:
-                grp_id[j].append(row[par])
+        if (row['obsid1'] in grp_id[j]) or (row['obsid2'] in grp_id[j]):
+            grp_edges[j].append({"node1" : row['obsid1'],
+                                "node2" : row['obsid2'],
+                                "dist_mag" : row['mag_dist'],
+                                "dist_flux" : row['flux_dist'],
+                                "dist_int" : row['int_dist']})
+            if row['obsid1'] not in grp_id[j]:
+                grp_id[j].append(row['obsid1'])
                 #grp[j].append([row[par], row['x1'], row['y1'], row['z1'],
                 #                row['f1'], row['n1']])
-                grp[j].append({"id" : row[par],
-                                "x" : row['x1'],
-                                "y" : row['y1'],
-                                "z" : row['z1'],
-                                "fuv" : row['f1'],
-                                "nuv" : row['n1']})
+                grp[j].append({"obsid" : row['obsid1'], "peid" : row['peid1'],
+                                "ra" : row['ra1'], "dec" : row['dec1'],
+                                "glat" : row['glat1'], "glon" : row['glon1'],
+                                "x" : row['x1'], "y" : row['y1'],
+                                "z" : row['z1'], "fuv_mag" : row['fuv_mag1'],
+                                "nuv_mag" : row['nuv_mag1'],
+                                "fuv_flux" : row['fuv_flux1'],
+                                "nuv_flux" : row['nuv_flux1'],
+                                "fuv_int" : row['fuv_int1'],
+                                "nuv_int" : row['nuv_int1']})
 
-            if row[nbh] not in grp_id[j]:
-                grp_id[j].append(row[nbh])
-                grp[j].append({"id" : row[nbh],
-                                "x" : row['x2'],
-                                "y" : row['y2'],
-                                "z" : row['z2'],
-                                "fuv" : row['f2'],
-                                "nuv" : row['n2']})
-
+            if row['obsid2'] not in grp_id[j]:
+                grp_id[j].append(row['obsid2'])
+                grp[j].append({"obsid" : row['obsid2'], "peid" : row['peid2'],
+                                "ra" : row['ra2'], "dec" : row['dec2'],
+                                "glat" : row['glat2'], "glon" : row['glon2'],
+                                "x" : row['x2'], "y" : row['y2'],
+                                "z" : row['z2'], "fuv_mag" : row['fuv_mag2'],
+                                "nuv_mag" : row['nuv_mag2'],
+                                "fuv_flux" : row['fuv_flux2'],
+                                "nuv_flux" : row['nuv_flux2'],
+                                "fuv_int" : row['fuv_int2'],
+                                "nuv_int" : row['nuv_int2']})
             flag = 1
+            break
+
     if flag == 0:
         new_grp_id = []
-        new_grp_id.append(row[par])
-        new_grp_id.append(row[nbh])
+        new_grp_id.append(row['obsid1'])
+        new_grp_id.append(row['obsid2'])
         grp_id.append(new_grp_id)
 
-        new_grp = []
-        new_grp.append({"id" : row[par],
-                        "x" : row['x1'],
-                        "y" : row['y1'],
-                        "z" : row['z1'],
-                        "fuv" : row['f1'],
-                        "nuv" : row['n1']})
+        new_grp_edges = []
+        new_grp_edges.append({"node1" : row['obsid1'],
+                            "node2" : row['obsid2'],
+                            "dist_mag" : row['mag_dist'],
+                            "dist_flux" : row['flux_dist'],
+                            "dist_int" : row['int_dist']})
+        grp_edges.append(new_grp_edges)
 
-        new_grp.append({"id" : row[nbh],
-                        "x" : row['x2'],
-                        "y" : row['y2'],
-                        "z" : row['z2'],
-                        "fuv" : row['f2'],
-                        "nuv" : row['n2']})
+        new_grp = []
+        new_grp.append({"obsid" : row['obsid1'], "peid" : row['peid1'],
+                        "ra" : row['ra1'], "dec" : row['dec1'],
+                        "glat" : row['glat1'], "glon" : row['glon1'],
+                        "x" : row['x1'], "y" : row['y1'],
+                        "z" : row['z1'], "fuv_mag" : row['fuv_mag1'],
+                        "nuv_mag" : row['nuv_mag1'],
+                        "fuv_flux" : row['fuv_flux1'],
+                        "nuv_flux" : row['nuv_flux1'],
+                        "fuv_int" : row['fuv_int1'],
+                        "nuv_int" : row['nuv_int1']})
+
+        new_grp.append({"obsid" : row['obsid2'], "peid" : row['peid2'],
+                        "ra" : row['ra2'], "dec" : row['dec2'],
+                        "glat" : row['glat2'], "glon" : row['glon2'],
+                        "x" : row['x2'], "y" : row['y2'],
+                        "z" : row['z2'], "fuv_mag" : row['fuv_mag2'],
+                        "nuv_mag" : row['nuv_mag2'],
+                        "fuv_flux" : row['fuv_flux2'],
+                        "nuv_flux" : row['nuv_flux2'],
+                        "fuv_int" : row['fuv_int2'],
+                        "nuv_int" : row['nuv_int2']})
+
         grp.append(new_grp)
 
 for i in range(len(grp_id)):
@@ -65,8 +98,11 @@ for i in range(len(grp_id)):
 for i in range(len(grp)):
     grp_json = {}
     grp_json["lvl1_grp_id"] = "grp"+str(i)
-    grp_json["obs"] = grp[i]
+    grp_json["nodes"] = grp[i]
+    grp_json["edges"] = grp_edges[i]
 
+    with open("../json_data_dump/grp"+str(i)+".json" , 'w') as outfile:
+        json.dump(grp_json, outfile)
     #n = len(grp_json["obs"])
     #grp_json["e_fuv"] = sum(smpl["fuv"] for smpl in grp_json["obs"])/n
     #grp_json["e_nuv"] = sum(smpl["nuv"] for smpl in grp_json["obs"])/n
